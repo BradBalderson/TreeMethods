7c25a360c2c274981af2d2a938b7553700a46d8c
bradbal <s4392586@student.uq.edu.au>
2019-11-04 15:17:31 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+This repository stores a basic implementation for creating a neighbour joining tree from a given distance or similarity matrix.
+
+Generating a distance matrix (A good way to do this is to use sklearn.DistanceMetrics with real data):
+''' Python
+from sklearn.neighbors import DistanceMetric
+
+dist = DistanceMetric.get_metric('euclidean')
+X = [[0, 1, 2],
+     [3, 4, 5],
+     [2, 3, 1],
+     [0, 2, 1]]
+dist_mat = dist.pairwise(X)
+'''
+
+Now that we have our distance matrix, we can now use construct a neighbour joining tree, giving some labels for the different
+samples:
+''' Python
+import numpy
+import TreeMethods.TreeBuild as TB
+
+tree = TB.njTree(dist_mat, numpy.array(['A', 'B', 'C', 'D']))
+'''
+
+We can then use ete3 to construct this into a tree object:
+''' Python
+from ete3 import Tree
+
+tree = Tree(tree)
+print(tree)
+
+   /-B
+  |
+  |   /-D
+--|--|
+  |   \-A
+  |
+   \-C
+'''
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requireFile = open("requirements.txt", 'r')
+requires = [line.strip('\n') for line in requireFile]
+#requires = [line.split('=')[0].split('>')[0].split('<')[0] for line in requireFile]
+
+setuptools.setup(
+    name="TreeMethods", # Replace with your own username
+    version="1.0.0",
+    author="Brad Balderson",
+    author_email="brad.balderson@uqconnect.edu.au",
+    description="Creating a neighbour joining tree.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BradBalderson/TreeMethods",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requires,
+    python_requires='>=3.6',
+)
